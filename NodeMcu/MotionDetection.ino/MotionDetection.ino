1bd4a70b18496f8c3a0d0e54627ebde5127e91f9
Peter Bons <msn@2000miles.nl>
2020-01-02 16:18:52 +0100

    Send mtqtt message on motion change

---
+++ NodeMcu/MotionDetection.ino/MotionDetection.ino
@@ -0,0 +1,83 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "secrets.h"
+
+#define wifi_ssid wifi_ssid_secret
+#define wifi_password wifi_password_secret
+
+#define mqtt_server "farmer.cloudmqtt.com"
+#define mqtt_port 11245 
+#define mqtt_user mqtt_user_secret
+#define mqtt_password mqtt_password_secret
+
+#define motion_topic "sensor/motion"
+
+const int STATUS_LED = 2;
+const int MOTION_DETECTOR = 12;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void connect_wifi() {
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(wifi_ssid);
+
+  WiFi.begin(wifi_ssid, wifi_password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void connect_mqtt() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP8266Client", mqtt_user, mqtt_password)) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" trying again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Setting things up.");
+
+  connect_wifi();
+
+  client.setServer(mqtt_server, mqtt_port);
+
+  pinMode(STATUS_LED, OUTPUT);
+  pinMode(MOTION_DETECTOR, INPUT);
+}
+
+long lastState = LOW;
+
+void loop() {
+  if (!client.connected()) {
+    connect_mqtt();
+  }
+  client.loop();
+  
+  long state = digitalRead(MOTION_DETECTOR);
+  if(state == lastState)
+    return;
+
+  digitalWrite (STATUS_LED, state == HIGH ? LOW : HIGH);
+  client.publish(motion_topic, state == HIGH ? "On" : "Off", true);
+
+  lastState = state;
+
+  delay(1000);
+}
