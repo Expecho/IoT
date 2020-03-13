a524a31ebbd1027a6f93897899d95af64ce3772b
Peter Bons <Expecho@users.noreply.github.com>
2020-03-13 11:45:03 +0100

    Update to latest beta (#2)
    * Clean unwanted code
    
    * Fix build errors, update tooling
    
    * Exclude build files

--- AzureSphere/MySphere/i2c.c
+++ AzureSphere/MySphere/i2c.c
@@ -1,35 +1,3 @@
-/*
- *  Some of the code in this file was copied from ST Micro.  Below is their required information.
- *
- * @attention
- *
- * <h2><center>&copy; COPYRIGHT(c) 2018 STMicroelectronics</center></h2>
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *   1. Redistributions of source code must retain the above copyright notice,
- *      this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in the
- *      documentation and/or other materials provided with the distribution.
- *   3. Neither the name of STMicroelectronics nor the names of its
- *      contributors may be used to endorse or promote products derived from
- *      this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- */
-
 #include <errno.h>
 #include <signal.h>
 #include <stdbool.h>
@@ -53,16 +21,9 @@
 #include "lps22hh_reg.h"
 
 /* Private variables ---------------------------------------------------------*/
-static axis3bit16_t data_raw_acceleration;
-static axis3bit16_t data_raw_angular_rate;
-static axis3bit16_t raw_angular_rate_calibration;
 static axis1bit32_t data_raw_pressure;
-static axis1bit16_t data_raw_temperature;
-static float acceleration_mg[3];
-static float angular_rate_dps[3];
-static float lsm6dsoTemperature_degC;
+
 static float pressure_hPa;
-static float lps22hhTemperature_degC;
 
 static uint8_t whoamI, rst;
 int accelTimerFd;
@@ -105,9 +66,8 @@ void AccelTimerEventHandler(EventData *eventData)
 	uint8_t reg;
 	lps22hh_reg_t lps22hhReg;
 
-#if (defined(IOT_CENTRAL_APPLICATION) || defined(IOT_HUB_APPLICATION))
 	static bool firstPass = true;
-#endif
+
 	// Consume the event.  If we don't do this we'll come right back 
 	// to process the same event again
 	if (ConsumeTimerFdEvent(accelTimerFd) != 0) {
@@ -115,57 +75,10 @@ void AccelTimerEventHandler(EventData *eventData)
 		return;
 	}
 
-	// Read the sensors on the lsm6dso device
-
-	//Read output only if new xl value is available
-	lsm6dso_xl_flag_data_ready_get(&dev_ctx, &reg);
-	if (reg)
-	{
-		// Read acceleration field data
-		memset(data_raw_acceleration.u8bit, 0x00, 3 * sizeof(int16_t));
-		lsm6dso_acceleration_raw_get(&dev_ctx, data_raw_acceleration.u8bit);
-
-		acceleration_mg[0] = lsm6dso_from_fs4_to_mg(data_raw_acceleration.i16bit[0]);
-		acceleration_mg[1] = lsm6dso_from_fs4_to_mg(data_raw_acceleration.i16bit[1]);
-		acceleration_mg[2] = lsm6dso_from_fs4_to_mg(data_raw_acceleration.i16bit[2]);
-
-		Log_Debug("\nLSM6DSO: Acceleration [mg]  : %.4lf, %.4lf, %.4lf\n",
-			acceleration_mg[0], acceleration_mg[1], acceleration_mg[2]);
-	}
-
-	lsm6dso_gy_flag_data_ready_get(&dev_ctx, &reg);
-	if (reg)
-	{
-		// Read angular rate field data
-		memset(data_raw_angular_rate.u8bit, 0x00, 3 * sizeof(int16_t));
-		lsm6dso_angular_rate_raw_get(&dev_ctx, data_raw_angular_rate.u8bit);
-
-		// Before we store the mdps values subtract the calibration data we captured at startup.
-		angular_rate_dps[0] = (lsm6dso_from_fs2000_to_mdps(data_raw_angular_rate.i16bit[0] - raw_angular_rate_calibration.i16bit[0])) / 1000.0;
-		angular_rate_dps[1] = (lsm6dso_from_fs2000_to_mdps(data_raw_angular_rate.i16bit[1] - raw_angular_rate_calibration.i16bit[1])) / 1000.0;
-		angular_rate_dps[2] = (lsm6dso_from_fs2000_to_mdps(data_raw_angular_rate.i16bit[2] - raw_angular_rate_calibration.i16bit[2])) / 1000.0;
-
-		Log_Debug("LSM6DSO: Angular rate [dps] : %4.2f, %4.2f, %4.2f\r\n",
-			angular_rate_dps[0], angular_rate_dps[1], angular_rate_dps[2]);
-
-	}
-
-	lsm6dso_temp_flag_data_ready_get(&dev_ctx, &reg);
-	if (reg)
-	{
-		// Read temperature data
-		memset(data_raw_temperature.u8bit, 0x00, sizeof(int16_t));
-		lsm6dso_temperature_raw_get(&dev_ctx, data_raw_temperature.u8bit);
-		lsm6dsoTemperature_degC = lsm6dso_from_lsb_to_celsius(data_raw_temperature.i16bit);
-
-		Log_Debug("LSM6DSO: Temperature  [degC]: %.2f\r\n", lsm6dsoTemperature_degC);
-	}
-
 	// Read the lps22hh sensor on the lsm6dso device
 
 	// Initialize the data structures to 0s.
-	memset(data_raw_pressure.u8bit, 0x00, sizeof(int32_t));
-	memset(data_raw_temperature.u8bit, 0x00, sizeof(int16_t));
+	//memset(data_raw_pressure.u8bit, 0x00, sizeof(int32_t));
 
 	if (lps22hhDetected) {
 		lps22hh_read_reg(&pressure_ctx, LPS22HH_STATUS, (uint8_t *)&lps22hhReg, 1);
@@ -178,22 +91,15 @@ void AccelTimerEventHandler(EventData *eventData)
 
 			pressure_hPa = lps22hh_from_lsb_to_hpa(data_raw_pressure.i32bit);
 
-			lps22hh_temperature_raw_get(&pressure_ctx, data_raw_temperature.u8bit);
-			lps22hhTemperature_degC = lps22hh_from_lsb_to_celsius(data_raw_temperature.i16bit);
-
 			Log_Debug("LPS22HH: Pressure     [hPa] : %.2f\r\n", pressure_hPa);
-			Log_Debug("LPS22HH: Temperature  [degC]: %.2f\r\n", lps22hhTemperature_degC);
 		}
 	}
 	// LPS22HH was not detected
 	else {
 
 		Log_Debug("LPS22HH: Pressure     [hPa] : Not read!\r\n");
-		Log_Debug("LPS22HH: Temperature  [degC]: Not read!\r\n");
 	}
 
-
-#if (defined(IOT_CENTRAL_APPLICATION) || defined(IOT_HUB_APPLICATION))
 
 		// We've seen that the first read of the Accelerometer data is garbage.  If this is the first pass
 		// reading data, don't report it to Azure.  Since we're graphing data in Azure, this data point
@@ -201,25 +107,22 @@ void AccelTimerEventHandler(EventData *eventData)
 		if (!firstPass) {
 
 			// Allocate memory for a telemetry message to Azure
-			char *pjsonBuffer = (char *)malloc(JSON_BUFFER_SIZE);
-			if (pjsonBuffer == NULL) {
-				Log_Debug("ERROR: not enough memory to send telemetry");
-			}
+			//char *pjsonBuffer = (char *)malloc(JSON_BUFFER_SIZE);
+			//if (pjsonBuffer == NULL) {
+			//	Log_Debug("ERROR: not enough memory to send telemetry");
+			//}
 
-			// construct the telemetry message
-			snprintf(pjsonBuffer, JSON_BUFFER_SIZE, "{\"gX\":\"%.4lf\", \"gY\":\"%.4lf\", \"gZ\":\"%.4lf\", \"pressure\": \"%.2f\", \"aX\": \"%4.2f\", \"aY\": \"%4.2f\", \"aZ\": \"%4.2f\"}",
-				acceleration_mg[0], acceleration_mg[1], acceleration_mg[2], pressure_hPa, angular_rate_dps[0], angular_rate_dps[1], angular_rate_dps[2]);
+			//// construct the telemetry message
+			//snprintf(pjsonBuffer, JSON_BUFFER_SIZE, "{\"gX\":\"%.4lf\", \"gY\":\"%.4lf\", \"gZ\":\"%.4lf\", \"pressure\": \"%.2f\", \"aX\": \"%4.2f\", \"aY\": \"%4.2f\", \"aZ\": \"%4.2f\"}",
+			//	pressure_hPa);
 
-			Log_Debug("\n[Info] Sending telemetry: %s\n", pjsonBuffer);
-			AzureIoT_SendMessage(pjsonBuffer);
-			free(pjsonBuffer);
+			//Log_Debug("\n[Info] Sending telemetry: %s\n", pjsonBuffer);
+			//AzureIoT_SendMessage(pjsonBuffer);
+			//free(pjsonBuffer);
 
 		}
 
 		firstPass = false;
-
-#endif 
-
 }
 
 /// <summary>
@@ -232,7 +135,7 @@ int initI2c(void) {
 
 	i2cFd = I2CMaster_Open(AVNET_MT3620_SK_ISU2_I2C);
 	if (i2cFd < 0) {
-		Log_Debug("ERROR: I2CMaster_Open: errno=%d (%s)\n", errno, strerror(errno));
+		//Log_Debug("ERROR: I2CMaster_Open: errno=%d (%s)\n", errno, strerror(errno));
 		return -1;
 	}
 
@@ -342,57 +245,6 @@ int initI2c(void) {
 			break;
 		}
 	}
-
-	// Read the raw angular rate data from the device to use as offsets.  We're making the assumption that the device
-	// is stationary.
-
-	uint8_t reg;
-	
-	Log_Debug("LSM6DSO: Calibrating angular rate . . .\n"); 
-	Log_Debug("LSM6DSO: Please make sure the device is stationary.\n");
-
-	do {
-
-		// Delay and read the device until we have data!
-		do {
-			// Read the calibration values
-			HAL_Delay(5000);
-			lsm6dso_gy_flag_data_ready_get(&dev_ctx, &reg);
-		} while (!reg);
-
-		if (reg)
-		{
-			// Read angular rate field data to use for calibration offsets
-			memset(data_raw_angular_rate.u8bit, 0x00, 3 * sizeof(int16_t));
-			lsm6dso_angular_rate_raw_get(&dev_ctx, raw_angular_rate_calibration.u8bit);
-		}
-
-		// Delay and read the device until we have data!
-		do {
-			// Read the calibration values
-			HAL_Delay(5000);
-			lsm6dso_gy_flag_data_ready_get(&dev_ctx, &reg);
-		} while (!reg);
-
-		// Read the angular data rate again and verify that after applying the calibration, we have 0 angular rate in all directions
-		if (reg)
-		{
-
-			// Read angular rate field data
-			memset(data_raw_angular_rate.u8bit, 0x00, 3 * sizeof(int16_t));
-			lsm6dso_angular_rate_raw_get(&dev_ctx, data_raw_angular_rate.u8bit);
-
-			// Before we store the mdps values subtract the calibration data we captured at startup.
-			angular_rate_dps[0] = lsm6dso_from_fs2000_to_mdps(data_raw_angular_rate.i16bit[0] - raw_angular_rate_calibration.i16bit[0]);
-			angular_rate_dps[1] = lsm6dso_from_fs2000_to_mdps(data_raw_angular_rate.i16bit[1] - raw_angular_rate_calibration.i16bit[1]);
-			angular_rate_dps[2] = lsm6dso_from_fs2000_to_mdps(data_raw_angular_rate.i16bit[2] - raw_angular_rate_calibration.i16bit[2]);
-
-		}
-
-	// If the angular values after applying the offset are not all 0.0s, then do it again!
-	} while ((angular_rate_dps[0] != 0.0) || (angular_rate_dps[1] != 0.0) || (angular_rate_dps[2] != 0.0));
-
-	Log_Debug("LSM6DSO: Calibrating angular rate complete!\n");
 
 	// Init the epoll interface to periodically run the AccelTimerEventHandler routine where we read the sensors
 
