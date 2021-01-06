04f7ae1d6f75cc28c63f980c058d77d99b75532e
Peter Bons <Expecho@users.noreply.github.com>
2021-01-06 14:09:13 +0100

    Update buildscript.sh

--- AzureSphere/MySphere/buildscript.sh
+++ AzureSphere/MySphere/buildscript.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sed "s|MY_CONNECTION_STRING|$MY_CONNECTION_STRING_ENV|g" /src/azurefunction.h > /src/transformed.h
+sed -i "s|MY_CONNECTION_STRING|$MY_CONNECTION_STRING_ENV|g" /src/azurefunction.h
